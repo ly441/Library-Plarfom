41a275c013527efdb18557fe7168490b411d0075
mutuajoseph <wambuajoseph756@gmail.com>
2025-05-23 15:28:33 +0300

    Implement CRUD operations for Book and Member resources

--- resources/book_resource.py
+++ resources/book_resource.py
@@ -4,21 +4,72 @@ from models.book import Book
 class BookResource:
 
     @staticmethod
-    def create():
-        pass
+    def create(self: Book):
+        with get_connection() as conn, conn.cursor() as cur:
+            cur.execute("""
+                INSERT INTO books (id, title, isbn, published_date, total_copies, available_copies ,category_id)
+                VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """,
+            (self.id, self.title, self.isbn, self.published_date, self.total_copies, self.available_copies, self.category_id)
+            )
+            print(f"{self.title} book has been added successfully!!")
+        return True
 
     @staticmethod
     def listall():
-        pass
+        with get_connection() as conn, conn.cursor() as cur:
+            cur.execute("SELECT * FROM books")
+            rows = cur.fetchall()
+            books = []
+            for row in rows:
+                book = Book(
+                    id=row[0],
+                    title=row[1],
+                    isbn=row[2],
+                    published_date=row[3],
+                    total_copies=row[4],
+                    available_copies=row[5],
+                    category_id=row[6]
+                )
+                books.append(book)
+        return books
 
     @staticmethod
-    def update():
-        pass
+    def update(self: Book):
+        with get_connection() as conn, conn.cursor() as cur:
+            cur.execute("""
+                UPDATE books
+                SET title = %s, isbn = %s, published_date = %s, total_copies = %s, available_copies = %s, category_id = %s
+                WHERE id = %s
+            """,
+            (self.title, self.isbn, self.published_date, self.total_copies, self.available_copies, self.category_id, self.id)
+            )
+            print(f"{self.title} book has been updated successfully!!")
+        return True
 
     @staticmethod
-    def delete():
-        pass
+    def delete(self: Book):
+        with get_connection() as conn, conn.cursor() as cur:
+            cur.execute("DELETE FROM books WHERE id = %s", (self.id,))
+            print(f"{self.title} book has been deleted successfully!!")
+        return True
 
     @staticmethod
-    def listone():
-        pass
+    def listone(self: Book):
+        with get_connection() as conn, conn.cursor() as cur:
+            cur.execute("SELECT * FROM books WHERE id = %s", (self.id,))
+            row = cur.fetchone()
+            if row:
+                book = Book(
+                    id=row[0],
+                    title=row[1],
+                    isbn=row[2],
+                    published_date=row[3],
+                    total_copies=row[4],
+                    available_copies=row[5],
+                    category_id=row[6]
+                )
+                return book
+            else:
+                print(f"Book with ID {self.id} not found.")
+        return None
--- resources/member_resource.py
+++ resources/member_resource.py
@@ -2,4 +2,72 @@ from db import get_connection
 from models.member import Member
 
 class MemberResource:
-    pass
+    
+    @staticmethod
+    def create(self: Member):
+        with get_connection() as conn, conn.cursor() as cur:
+            cur.execute("""
+                INSERT INTO members (id, first_name, last_name, surname, email, joined_date)
+                VALUES (%s, %s, %s, %s, %s, %s)
+            """,
+            (self.id, self.first_name, self.last_name, self.surname, self.email, self.joined_date)
+            )
+            print(f"{self.first_name} {self.last_name} has been added successfully!!")
+        return True
+    
+    @staticmethod
+    def listall():
+        with get_connection() as conn, conn.cursor() as cur:
+            cur.execute("SELECT * FROM members")
+            rows = cur.fetchall()
+            members = []
+            for row in rows:
+                member = Member(
+                    id=row[0],
+                    first_name=row[1],
+                    last_name=row[2],
+                    surname=row[3],
+                    email=row[4],
+                    joined_date=row[5]
+                )
+                members.append(member)
+        return members
+    
+    @staticmethod
+    def update(self: Member):
+        with get_connection() as conn, conn.cursor() as cur:
+            cur.execute("""
+                UPDATE members
+                SET first_name = %s, last_name = %s, surname = %s, email = %s, joined_date = %s
+                WHERE id = %s
+            """,
+            (self.first_name, self.last_name, self.surname, self.email, self.joined_date, self.id)
+            )
+            print(f"{self.first_name} {self.last_name} has been updated successfully!!")
+        return True
+    
+    @staticmethod
+    def delete(self: Member):
+        with get_connection() as conn, conn.cursor() as cur:
+            cur.execute("DELETE FROM members WHERE id = %s", (self.id,))
+            print(f"{self.first_name} {self.last_name} has been deleted successfully!!")
+        return True
+    
+    @staticmethod
+    def listone(self: Member):
+        with get_connection() as conn, conn.cursor() as cur:
+            cur.execute("SELECT * FROM members WHERE id = %s", (self.id,))
+            row = cur.fetchone()
+            if row:
+                member = Member(
+                    id=row[0],
+                    first_name=row[1],
+                    last_name=row[2],
+                    surname=row[3],
+                    email=row[4],
+                    joined_date=row[5]
+                )
+                return member
+            else:
+                print("Member not found")
+                return None
